76441de7834d1524befe5d7f568b743497d6c15f
Omdanii <mahmmoud.hassanein@gmail.com>
2025-04-06 23:15:39 +0200

    Time: 15 ms (43.27%), Space: 18 MB (33.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,22 @@
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        return Counter(s) == Counter(t)
+        scount, tcount = {}, {}
+        for i in range(len(s)):
+            if s[i] in scount:
+                scount[s[i]] += 1
+            else:
+                scount[s[i]] = 1
+
+            if t[i] in tcount:
+                tcount[t[i]] += 1
+            else:
+                tcount[t[i]] = 1
+
+        # We have to check if both containing the same chars
+        for k, v in scount.items():
+            if tcount.get(k) and tcount[k] == v:
+                continue
+            return False
+        return True
