029253918e94fa0d4e41c7f6eb6014aafecbc30f
Omdanii <mahmmoud.hassanein@gmail.com>
2025-04-07 18:19:16 +0200

    Time: 3 ms (52.53%), Space: 17.9 MB (18.48%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.py
+++ 3636-check-balanced-string/3636-check-balanced-string.py
@@ -1,13 +1,12 @@
 class Solution:
     def isBalanced(self, num: str) -> bool:
-        even_numbers = []
-        odd_numbers = []
+        even = 0
+        odd = 0
 
         for i in range(len(num)):
-            if i % 2 == 0:
-                odd_numbers.append(int(num[i]))
+            if (i & 1) == 0:
+                even += int(num[i])
             else:
-                even_numbers.append(int(num[i]))
-
-        is_balanced = sum(even_numbers) == sum(odd_numbers)
-        return is_balanced
+                odd += int(num[i])
+        return odd == even
+        
