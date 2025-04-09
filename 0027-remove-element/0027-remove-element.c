93b440da9a297974d9ad044c3969a886dd547891
Omdanii <mahmmoud.hassanein@gmail.com>
2025-04-09 19:37:50 +0200

    Time: 0 ms (100%), Space: 9.9 MB (13.25%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,15 @@
+int removeElement(int *nums, int numsSize, int val)
+{
+    int i = 0;
+    int j = 0;
+    while (i < numsSize)
+    {
+        if (nums[i] != val)
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+        i++;
+    }
+    return j;
+}
