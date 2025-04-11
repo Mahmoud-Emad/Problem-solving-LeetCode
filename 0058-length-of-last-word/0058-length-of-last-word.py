09fa43785e1ca13a0614a841c7125e149d33df8c
Omdanii <mahmmoud.hassanein@gmail.com>
2025-04-11 02:27:31 +0200

    Time: 0 ms (100%), Space: 17.8 MB (60.96%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -13,10 +13,7 @@ class Solution:
                 word += i
                 
         # Check last word
-        for s in word:
-            if s == ' ':
-                break
-            
+        if word:
             words.append(word)
-        
+                
         return len(words[-1])
