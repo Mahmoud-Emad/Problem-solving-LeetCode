75a51bbd93cf42a234b480dce2a278f7f6a0df6e
Omdanii <mahmmoud.hassanein@gmail.com>
2025-04-12 01:43:15 +0200

    Time: 0 ms (100%), Space: 4 MB (29.25%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.go
@@ -0,0 +1,14 @@
+func plusOne(digits []int) []int {
+	n := len(digits)
+
+	for i := n - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+
+	// If all digits were 9
+	return append([]int{1}, digits...)
+}
